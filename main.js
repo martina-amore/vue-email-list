8c44bc827f3b7d03c3abebee4ddd3c03218d124e
Martina Amore <martina.amore02@gmail.com>
2020-11-30 17:37:41 +0100

    Completo

---
+++ main.js
@@ -0,0 +1,13 @@
+const myApp = new Vue ({
+    el:"#root",
+    data: {
+        mails: [],
+    },
+    mounted() {
+        for (i = 0; i < 10; i++) {
+            axios.get("https://flynn.boolean.careers/exercises/api/random/mail").then((result) => {
+                this.mails.push(result.data.response)
+            });
+        }
+    }
+})
